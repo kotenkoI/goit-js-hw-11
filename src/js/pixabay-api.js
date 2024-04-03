f4011996c5c810e1718491005ab8390a7269a7a8
kotenkoI <iryna.herashchenko@gmail.com>
2024-04-03 14:02:59 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,7 +2,7 @@ import { galleryList } from '../main';
 import { query } from '../main';
 export function fetchImages(query) {
     return fetch(
-        `https://pixabay.com/api/?key=43130962-c664da34f6898978b873c66fd&${query}&image_type=photo&orientation=horizontal&safesearch=true`
+        `https://pixabay.com/api/?key=43130962-c664da34f6898978b873c66fd&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
     ).then(response => {
         if (!response.ok) {
             throw new Error(response.status);
