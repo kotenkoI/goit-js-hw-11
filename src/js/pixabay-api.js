096f86268d0dfbaad062cca3e1d017de66d40d14
kotenkoI <iryna.herashchenko@gmail.com>
2024-04-04 09:33:41 +0300

    changed

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,25 +1,9 @@
-// export function fetchImages(query) {
-//     return fetch(
-//         `https://pixabay.com/api/?key=43130962-c664da34f6898978b873c66fd&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-//     ).then(response => {
-//         if (!response.ok) {
-//             throw new Error(response.status);
-//         }
-//         return response.json();
-//     });
-// }
-export function fetchImages(query) {
-    const wayKey = "43130962-c664da34f6898978b873c66fd";
-    const params = new URLSearchParams({
-        key: wayKey,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-    });
-      const baseUrl = 'https://pixabay.com/api/';
-    const url = `${baseUrl}?${params}`;
-    return fetch(url).then(response => {
-    return response.json();
-  });
+const API_KEY = '42710952-cb07850fe6c5f6774b64d780f';
+const baseURL = 'https://pixabay.com/api/';
+
+export async function fetchImages(searchQuery) {
+  const url = `${baseURL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.hits;
 }
